dee11176fdf6b22649c3c6dee0a861925f4a7d73
dorian <dorian.vidal@hetic.net>
2023-07-05 14:16:47 +0200

    fix bug css

--- components/Common/Card.vue
+++ components/Common/Card.vue
@@ -2,6 +2,7 @@
   <div class="flex justify-center content-center">
     <div
       class="
+        card__section
         max-w-sm
         p-6
         bg-white
@@ -11,58 +12,76 @@
         dark:bg-gray-800 dark:border-gray-700
       "
     >
-      <a href="#">
-        <h5
-          class="
-            mb-2
-            text-2xl
-            font-bold
-            tracking-tight
-            text-gray-900
-            dark:text-white
-          "
-        >
-          Noteworthy technology acquisitions 2021
-        </h5>
-      </a>
-      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-        Here are the biggest enterprise technology acquisitions of 2021 so far,
-        in reverse chronological order.
-      </p>
-      <a
-        href="#"
+      <div
         class="
-          inline-flex
-          items-center
-          px-3
-          py-2
-          text-sm
-          font-medium
-          text-center text-white
-          bg-blue-700
-          rounded-lg
-          hover:bg-blue-800
-          focus:ring-4 focus:outline-none focus:ring-blue-300
-          dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800
+          mb-2
+          text-2xl
+          font-bold
+          tracking-tight
+          text-white
+          border-b-2 border-neutral-100
+          px-6
+          py-3
+          dark:border-neutral-600 dark:text-neutral-50
         "
       >
-        Read more
-        <svg
-          class="w-3.5 h-3.5 ml-2"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 14 10"
-        >
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M1 5h12m0 0L9 1m4 4L9 9"
+        Connexion
+      </div>
+      <div>
+        <div>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            class="
+              bg-gray-50
+              border border-gray-300
+              text-gray-900 text-sm
+              rounded-lg
+              focus:ring-blue-500 focus:border-blue-500
+              block
+              w-full
+              p-2.5
+              dark:text-white
+            "
+            placeholder="name@company.com"
+            required
           />
-        </svg>
-      </a>
+          <p class="mb-3 font-normal text-white">
+            Un email sera envoyé à l'adress indiqué afin de vous identifier.
+            Merci de consulter également vos spams.
+          </p>
+        </div>
+      </div>
+
+      <div
+        class="
+          border-t-2 border-neutral-100
+          px-6
+          py-3
+          dark:border-neutral-600 dark:text-neutral-50
+        "
+      >
+        <a
+          href="#"
+          class="
+            inline-flex
+            items-center
+            px-3
+            py-2
+            text-sm
+            font-medium
+            text-center text-white
+            bg-blue-700
+            rounded-lg
+            hover:bg-blue-800
+            focus:ring-4 focus:outline-none focus:ring-blue-300
+            dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800
+          "
+        >
+          ENVOYEZ !
+        </a>
+      </div>
     </div>
   </div>
 </template>
@@ -127,4 +146,8 @@ export default {
 };
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.card__section {
+  background-color: #32325a !important;
+}
+</style>
